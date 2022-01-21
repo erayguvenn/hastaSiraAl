44b70fc4dcfba1cce537480d65649a6a921ab3c0
Eray <eray0736@gmail.com>
2022-01-21 14:10:22 +0300

    değişiklik
    verileri kaydetme

--- hastane/hastane/Program.cs
+++ hastane/hastane/Program.cs
@@ -12,16 +12,25 @@ using System.Threading.Tasks;
 namespace hastane
 {
 
-
-    public class doktorHasta
+    /// <summary>
+    /// doktor ve hasta bilgilerini tuttuğumuz listelerin bulunduğu bir sınıf
+    /// </summary>
+    public static class doktorHasta
     {
-        public IList <Doktor> doktor { get; set; }
-        public IList<hasta> hastalar { get; set; }
+        public static IList<Doktor> doktor { get; set; }
+        public static IList<hasta> hastalar { get; set; }
 
     }
+    /// <summary>
+    /// programın ana sınıfı 
+    /// </summary>
     public class Program
     {
-
+        /// <summary>
+        /// main metodu program çalıştığında ilk çalışan yerdir.
+        /// doktor  ve hasta bilgilerini bu metod içinde tanımlıyor
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             List<Doktor> doktorlar = new List<Doktor>();
@@ -29,23 +38,23 @@ namespace hastane
             poliklinik dahiliye1 = new dahiliye(1);
             poliklinik ortopedi1 = new ortopedi(1);
             kardiyoloji kardiyoloji1 = new kardiyoloji(1);
-
-            Doktor doktor1 =(new Doktor("ömer faruk küpeli", dahiliye1));
-            Doktor doktor2 =(new Doktor("eray güven", ortopedi1));
+            // doktornesneleri oluşturup yeni doktorlar ekliyor
+            Doktor doktor1 = (new Doktor("ömer faruk küpeli", dahiliye1));
+            Doktor doktor2 = (new Doktor("eray güven", ortopedi1));
             Doktor doktor3 = new Doktor("tahir bekem", kardiyoloji1);
 
-            hasta hasta1 = new hasta(11, "aykut", "kara",dahiliye1, dahiliye1.sıra_al(),doktor1.isim);
-            hasta hasta2 = new hasta(12, "sezgin", "sevinç",dahiliye1, dahiliye1.sıra_al(),doktor1.isim);
-            hasta hasta3 = new hasta(14, "ömer", "kayaoğlu",ortopedi1, ortopedi1.sıra_al(),doktor2.isim);
-            hasta hasta4 = new hasta(15, "ismail", "güven", kardiyoloji1, kardiyoloji1.sıra_al(),doktor3.isim);
+            // hasta nesneleri oluşturup hastalara sıra verip hasta kayıtları oluşturuyor
+            hasta hasta1 = new hasta(11, "aykut", "kara", dahiliye1, dahiliye1.sıra_al(), doktor1.isim);
+            hasta hasta2 = new hasta(12, "sezgin", "sevinç", dahiliye1, dahiliye1.sıra_al(), doktor1.isim);
+            hasta hasta3 = new hasta(14, "ömer", "kayaoğlu", ortopedi1, ortopedi1.sıra_al(), doktor2.isim);
+            hasta hasta4 = new hasta(15, "ismail", "güven", kardiyoloji1, kardiyoloji1.sıra_al(), doktor3.isim);
             hasta hasta5 = new hasta(12, "kazım", "koçyiğit", dahiliye1, dahiliye1.sıra_al(), doktor1.isim);
-            hasta hasta6 = new hasta(12, "ali kaan", "öztürk", ortopedi1, ortopedi1.sıra_al(),doktor2.isim);
+            hasta hasta6 = new hasta(12, "ali kaan", "öztürk", ortopedi1, ortopedi1.sıra_al(), doktor2.isim);
 
 
 
 
-
-            doktorHasta doktorHasta = new doktorHasta();
+            // doktar ve hasta listesine oluşturulan değerler ekleniyor.
             List<hasta> h = new List<hasta>();
             List<Doktor> d = new List<Doktor>();
             d.Add(doktor1);
@@ -63,32 +72,50 @@ namespace hastane
             doktorHasta.doktor = d;
             doktorHasta.hastalar = h;
 
-            //string json = JsonConvert.SerializeObject(doktorHasta);
-            //string yol = @"D:\D Bilgiler\ders\jsonCevir\hasta.json";
+            //kaydet fonksiyonu çalıştırılıyor.
+            //kaydedilen yol başka bilgisayarda değiştirilmeden çalıştırıldığında hata vereceği için yorum satırı haline getirdim.
+            //kaydet();
 
-            //using (var tw = new StreamWriter(yol, true))
-            //{
-            //    tw.WriteLine(json.ToString());
-            //    tw.Close();
-            //}
-
-            string json = JsonConvert.SerializeObject(doktorHasta);
-            string yol = @"D:\D Bilgiler\ders\jsonCevir\hasta.json";
-
-                using (var tw = new StreamWriter(yol, true))
-                {
-                    tw.WriteLine(json.ToString());
-                    tw.Close();
-                }
-                
 
             Console.ReadLine();
         }
 
-       
+        /// <summary>
+        /// bu fonksyon doktorHasta sınıfından doktor ve hasta verilerini ayrı ayrı bir json dosyası halinde kaydedilmesini sağlar.
+        /// </summary>
+        public static void kaydet()
+        {
+
+            string json = JsonConvert.SerializeObject(doktorHasta.doktor);
+            string json2 = JsonConvert.SerializeObject(doktorHasta.hastalar);
+
+            string yol = @"C:\Users\User\source\repos\hastane\hasta.json";
+
+            using (var tw = new StreamWriter(yol, true))
+            {
+                tw.WriteLine(json2.ToString());
+
+                tw.Close();
+            }
+            string yol2 = @"C:\Users\User\source\repos\hastane\doktor.json";
+            using (var tw = new StreamWriter(yol2, true))
+            {
+                tw.WriteLine(json2.ToString());
+
+                tw.Close();
+            }
+
+
+
+        }
+
+
 
     }
-
+    /// <summary>
+    /// doktorların özelliklerinin belirlendiği ana doktor sınıfı
+    /// doktorlara ait isim ve poliklinik bilgisi tutar
+    /// </summary>
     public class Doktor
     {
         public string isim { get; set; }
@@ -101,7 +128,10 @@ namespace hastane
         }
 
     }
-
+    /// <summary>
+    /// hastalar için gerekli alanların tanımlandığı kısımdır.
+    /// hastalar için tc,sıra,ad,soyad,polikllinik ve doktor bilgileri ister
+    /// </summary>
     public class hasta
     {
         public int tc { get; set; }
@@ -111,8 +141,16 @@ namespace hastane
         public poliklinik hasta_pol { get; set; }
         public string dok { get; set; }
 
-
-        public hasta(int tc , string ad,string soyad, poliklinik hasta_pol, int sira,string dok)
+        /// <summary>
+        /// hasta sınıfının kurucu metodudur ve dışardan gelen bilgileri kontrol eder.
+        /// </summary>
+        /// <param name="tc"></param>
+        /// <param name="ad"></param>
+        /// <param name="soyad"></param>
+        /// <param name="hasta_pol"></param>
+        /// <param name="sira"></param>
+        /// <param name="dok"></param>
+        public hasta(int tc, string ad, string soyad, poliklinik hasta_pol, int sira, string dok)
         {
             this.tc = tc;
             this.sira = sira;
@@ -121,25 +159,31 @@ namespace hastane
             this.hasta_pol = hasta_pol;
             this.dok = dok;
 
-            Console.WriteLine("hasta ad soyad : {0} {1} , hasta sıra : {2} , poliklinik : {3} , doktor : {4} ", ad, soyad, sira,hasta_pol,dok);
+            // sıra alan hastaları ekrana yazdırır.
+            Console.WriteLine("hasta ad soyad : {0} {1} , hasta sıra : {2} , poliklinik : {3} , doktor : {4} ", ad, soyad, sira, hasta_pol, dok);
             Console.WriteLine("________________________________________________________________________________________________________________");
         }
 
     }
-    
+    /// <summary>
+    /// poliklinik özelliklerinin ve hangi değişkenleri alacağının belirlendiği ana poliklinik sınıfı
+    /// her polikliniğin kendine ait sırası olduğu için güncel sırayı tutmaktadır.
+    /// </summary>
     public abstract class poliklinik
     {
         public int sıra { get; set; }
-        public abstract int  pol_id { get; set; }
+        public abstract int pol_id { get; set; }
         public abstract int sıra_al();
-        public abstract int mevcut_sira{ get; set; }
+        public abstract int mevcut_sira { get; set; }
 
     }
-
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır dahiliye polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class dahiliye : poliklinik
     {
-        public override int pol_id { get ; set ; }
-        public override int mevcut_sira { get ; set ; }
+        public override int pol_id { get; set; }
+        public override int mevcut_sira { get; set; }
 
         public dahiliye(int id)
         {
@@ -152,6 +196,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır ortopedi polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class ortopedi : poliklinik
     {
         public override int pol_id { get; set; }
@@ -166,6 +213,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır kardiyoloji polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class kardiyoloji : poliklinik
     {
         public override int pol_id { get; set; }
@@ -180,6 +230,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır genelcerrahi polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class genelCerrahi : poliklinik
     {
         public override int pol_id { get; set; }
@@ -194,6 +247,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır kadınHastalıklarıVeDoğum polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class kadınHastalıklarıVeDoğum : poliklinik
     {
         public override int pol_id { get; set; }
@@ -208,6 +264,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır cildiye polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class cildiye : poliklinik
     {
         public override int pol_id { get; set; }
@@ -222,6 +281,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır beyinCerrahisi polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class beyinCerrahisi : poliklinik
     {
         public override int pol_id { get; set; }
@@ -236,6 +298,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır gözHastalıkları polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class gözHastalıkları : poliklinik
     {
         public override int pol_id { get; set; }
@@ -250,6 +315,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır plastikCerrahi polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class plastikCerrahi : poliklinik
     {
         public override int pol_id { get; set; }
@@ -264,6 +332,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır kulakBurunBoğaz polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class kulakBurunBoğaz : poliklinik
     {
         public override int pol_id { get; set; }
@@ -278,6 +349,9 @@ namespace hastane
             return this.mevcut_sira++;
         }
     }
+    /// <summary>
+    /// poliklinik sınıfından miras almıştır ağızVeDişSağlığı polikliniği için kullanılan değerleri ve mevcut sırayı tutmaktadır.
+    /// </summary>
     public class ağızVeDişSağlığı : poliklinik
     {
         public override int pol_id { get; set; }
